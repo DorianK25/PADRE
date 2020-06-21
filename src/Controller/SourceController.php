731b7cd268cdd03aa3f999d120fe2d9eadc42a37
df <dorian.keur>
2020-06-21 11:43:44 +0200

    page source

--- src/Controller/SourceController.php
+++ src/Controller/SourceController.php
@@ -3,12 +3,12 @@
 
 namespace App\Controller;
 
-use App\Entity\Capacite;
 use App\Entity\Competence;
-use App\Form\ProfType;
+use App\Entity\Competence_tp;
+use App\Entity\Tp;
 use App\Repository\CapaciteRepository;
+use App\Repository\Competence_tpRepository;
 use App\Repository\CompetenceRepository;
-use App\Repository\SourceRepository;
 use App\Repository\TpRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -26,12 +26,17 @@ class SourceController extends AbstractController
     /**
      * @Route("/index", name="source_index")
      */
-    public function index(Request $request,CapaciteRepository $capacitesRepo,CompetenceRepository $competencesRepo,TpRepository $tpsRepo): Response
+    public function index(Request $request,CapaciteRepository $capacitesRepo,CompetenceRepository $competencesRepo,TpRepository $tpsRepo,Competence_tpRepository $compTpRepo): Response
     {
 
         $tps=$tpsRepo->findAll();
         $competences_=$competencesRepo->findBy([],["capacite"=>'ASC']);
-        $capacites_=$capacitesRepo->findAll();        
+        $capacites_=$capacitesRepo->findAll();
+        $competencesTp_=$compTpRepo->findAll();
+        foreach($competencesTp_ as $competenceTp){
+            $competencesTp[$competenceTp->getCompetence()->getId()][$competenceTp->getTp()->getId()][]=$competenceTp;
+
+        }        
         foreach($competences_ as $competence){
             $competences[$competence->getCapacite()->getId()][]=$competence;
 
@@ -40,15 +45,47 @@ class SourceController extends AbstractController
             $capacites[$capacite->getId()][]=$capacite;
 
         }
-        dump($tps);
-        dump($competences);
-        dump($capacites);
+        
 
         return $this->render('source/sourceIndex.html.twig', [
             "tps"=>$tps,
             "capacites"=>$capacites,
-            "competences"=>$competences
+            "competences"=>$competences,
+            "competencesTp"=>$competencesTp
         ]);
+    }
+    /**
+     * @Route("/create", name="source_create")
+     * 
+     */
+    public function create(CompetenceRepository $competencesRepo,Competence_tpRepository $compTpRepo,TpRepository $tpsRepo,Request $request)
+    {
+
+        $competenceTp=new Competence_tp();
+        $entityManager = $this->getDoctrine()->getManager();
+        $tp=$tpsRepo->find($request->get("idTp"));
+        $competence=$competencesRepo->find($request->get("idCompetence"));
+        $competenceTp->setTp($tp)->setCompetence($competence)->setBarem_competence($request->get("barem"));
+        $competenceTpbis=$compTpRepo->findOneBy(["tp" => $competenceTp->getTp(),"competence"=>$competenceTp->getCompetence()]);
+        if($competenceTp->getBarem_competence()!=0)
+            if($competenceTpbis){
+
+                if($competenceTpbis->getBarem_competence()!=$competenceTp->getBarem_competence()){
+                    $competenceTpbis->setBarem_competence($competenceTp->getBarem_competence());
+                    $entityManager->flush();
+
+                }
+            }else{
+                $entityManager->persist($competenceTp);
+                $entityManager->flush();
+            }
+
+
+
+        
+
+        return new Response("success");
+        
     }
 
     
