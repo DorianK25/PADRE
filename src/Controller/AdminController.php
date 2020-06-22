aed9aa4167571c8aa1d1b8f7064fc4ddb35fc1af
Théodorine Branlant <t.branlant@a2si.net>
2020-06-22 22:16:36 +0200

    add: formAdmin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,8 @@
 
 namespace App\Controller;
 
-use App\Form\ProfType;
+use App\Form\AdminType;
+use App\Repository\Mot_de_passe_adminRepository;
 use App\Repository\ProfesseurRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,12 +21,34 @@ class AdminController extends AbstractController
 
     
     /**
-     * @Route("/", name="mainAdmin")
+     * @Route("/index", name="indexAdmin")
      */
     public function index(Request $request)
     {
 
 
-        return $this->render('admin/formAdmin.html.twig');
+        return $this->render('admin/MainAdmin.html.twig');
+    }
+
+    /**
+     * @Route("/",name="formAdmin" )
+     */ 
+    public function formulaire(Request $request,Mot_de_passe_adminRepository $mdpRepo){
+
+        $form=$this->createForm(AdminType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if($form->getData()["mdp"]==$mdpRepo->find(1)->getMot_de_passe()){
+                return $this->redirectToRoute("indexAdmin");
+            }
+
+        
+        }
+
+        return $this->render('admin/formAdmin.html.twig',[
+            "form"=>$form->createView(),
+            
+        ]);
     }
 }
